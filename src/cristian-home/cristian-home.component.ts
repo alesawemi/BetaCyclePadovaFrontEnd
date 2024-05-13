b46c48ef569cd3f01e43727ff0448bb7222327a8
alesawemi <alessio.tamburrano1994@gmail.com>
2024-05-13 17:38:26 +0200

    Update Home

--- src/cristian-home/cristian-home.component.ts
+++ src/cristian-home/cristian-home.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 @Component({
   selector: 'app-cristian-home',
   standalone: true,
   imports: [],
   templateUrl: './cristian-home.component.html',
-  styleUrl: './cristian-home.component.css'
+  styleUrl: './cristian-home.component.css',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class CristianHomeComponent {
 
