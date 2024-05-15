797df80ef21cbf5c728260e88aaa0bdab51514a2
MartinaBonaventura <bonaventura.martina@gmail.com>
2024-05-15 20:14:22 +0200

    qualche progresso per la ricerca (per ora Accessories ma facilmente adattabile agli altri components)
    Non funziona ancora al 100%, se si selezionano più colori/categorie per ora prende soltanto il primo. Manca ancora filtro per prezzo (a parte ordine crescente/decrescente)

--- src/app/features/accessories/accessories.component.ts
+++ src/app/features/accessories/accessories.component.ts
@@ -4,6 +4,7 @@ import { FormsModule, ReactiveFormsModule, NgModel } from '@angular/forms';
 import { CommonService } from '../../shared/services/common.service';
 import { Accessory } from '../../shared/models/accessoriesData';
 import { Filters } from '../../shared/models/productsFilters';
+import { Properties } from '../../shared/models/productProperties';
 
 @Component({
   selector: 'app-accessories',
@@ -22,6 +23,15 @@ export class AccessoriesComponent {
   GetAllAccessories(){
     this.priceAscending = false;
     this.priceDescending = false;
+
+    for (const col of Object.keys(this.selectedColors)) { this.selectedColors[col] = false; }
+    this.selectedColors = {};
+    this.filtersFromInput.color = 'color';
+    
+    for (const cat of Object.keys(this.selectedCategories)) { this.selectedCategories[cat] = false; }
+    this.selectedCategories= {};
+    this.filtersFromInput.productCategory = 'category';
+
     this.http.GetAll(`https://localhost:7228/api/AccessoriesViews`)
     .subscribe({
         next: (Data: any) => { 
@@ -34,12 +44,23 @@ export class AccessoriesComponent {
       })
   }
 
-  filtersOn: boolean = false;
+  accessoryProperties: Properties = new Properties();
 
-  openFilters() {
-    this.filtersOn = !this.filtersOn;
+  GetResearchOptions(){
+    this.http.GetProperties(`https://localhost:7228/api/AccessoriesViews/GetProductProperties`)
+    .subscribe({
+        next: (Data: any) => { 
+          this.accessoryProperties.availableColors = Data.availableColors.$values;
+          this.accessoryProperties.availableCategories = Data.availableCategories.$values;
+          this.accessoryProperties.priceMax = Data.priceMax;
+          this.accessoryProperties.priceMin = Data.priceMin;
+        },
+        error: (errore: any) => {
+          console.log(errore);
+        }
+      })
   }
-  
+   
   priceAscending: boolean = false;
   priceDescending: boolean = false;
   
@@ -59,7 +80,6 @@ export class AccessoriesComponent {
     else this.GetAllAccessories();
   }
 
-
   ifAscending(event: any){
     if (event.target.checked) {
       this.http.GetAll(`https://localhost:7228/api/AccessoriesViews/OrderByPriceAscending`)
@@ -77,15 +97,37 @@ export class AccessoriesComponent {
   }
 
   filtersFromInput: Filters = new Filters();
+  
+  selectedColors: { [key: string]: boolean } = {};
+  selectedCategories: { [key: string]: boolean } = {};
 
   GetAccessoriesWithFilters(){
 
-    this.filtersFromInput.productName = 'lights';    
-    this.filtersFromInput.color = 'color'; 
+    if (Object.keys(this.selectedColors).length > 0) {
+      this.filtersFromInput.color = '';
+      for (const color of Object.keys(this.selectedColors)) {
+        if (this.selectedColors[color]) {
+          this.filtersFromInput.color = this.filtersFromInput.color.concat(":" + color)
+        }
+      }
+    }    
+
+    if (Object.keys(this.selectedCategories).length > 0) {
+      this.filtersFromInput.productCategory = '';
+      for (const cat of Object.keys(this.selectedCategories)) {
+        if (this.selectedCategories[cat]) {
+          this.filtersFromInput.productCategory = this.filtersFromInput.productCategory.concat(":" + cat)
+        }
+      }
+    }
+    
+
+    this.filtersFromInput.productName = 'allProducts';    
+    //this.filtersFromInput.color = 'red'; 
     this.filtersFromInput.size = 'size';  
-    this.filtersFromInput.productCategory = 'category';
+    //this.filtersFromInput.productCategory = 'category';
     this.filtersFromInput.maxPrice = 0.;
-    this.filtersFromInput.minPrice = 5.;
+    this.filtersFromInput.minPrice = 0.;
     this.filtersFromInput.maxWeight = 0;
     this.filtersFromInput.minWeight = 0.;
     this.filtersFromInput.ascPrice = this.priceAscending;
@@ -106,7 +148,8 @@ export class AccessoriesComponent {
 
 
   ngOnInit() {
-    this.GetAllAccessories()
+    this.GetAllAccessories();
+    this.GetResearchOptions();
   }
 
 }
